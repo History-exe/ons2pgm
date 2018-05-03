6f96567dd5e3783150255f1b2f7a65715e1003c0
lsby <2451759073@qq.com>
2018-05-04 02:16:30 +0800

    初始提交

---
+++ inedx.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var onsStr = fs.readFileSync('./ons.txt').toString()
+
+var onsArr = onsStr.split('\n')
+var pgmArr = []
+
+for (var i = 0; i < onsArr.length; i++) {
+    var row = onsArr[i]
+
+    if (row.substring(0, 5) == 'music') {
+        var name = row.match(/(?<=".*",").*(?=")/)[0]
+        pgmArr.push('bgm("' + name + '.mp3")')
+    }
+}
+console.log(pgmArr)
