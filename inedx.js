564c527f64acba341d4fe263160edd43ec61ae85
lsby <2451759073@qq.com>
2018-05-04 02:35:33 +0800

    分文件

--- inedx.js
+++ inedx.js
@@ -1,16 +1,13 @@
 var fs = require('fs')
+var music = require('./cmd/music')
 
 var onsStr = fs.readFileSync('./ons.txt').toString();
-
 var onsArr = onsStr.split('\n')
 var pgmArr = []
 
 for (var i = 0; i < onsArr.length; i++) {
     var row = onsArr[i]
 
-    if (row.substring(0, 5) == 'music') {
-        var name = row.match(/(?<=".*",").*(?=")/)[0]
-        pgmArr.push('bgm("' + name + '.mp3")')
-    }
+    music(row, pgmArr)
 }
 console.log(pgmArr)
