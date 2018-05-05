b74e46c0d586e7d353199c6a3167b8150ba6c7cb
History_exe <33642965+History-exe@users.noreply.github.com>
2018-05-05 19:19:29 +0800

    显示立绘 测试

---
+++ cmd/figure_on.js
@@ -0,0 +1,13 @@
+module.exports = function (row, pgmArr) {
+    if (row.substring(0, 6) == 'chara0') {
+        var name = row.match(/(?<="file4",").*(?=")/)
+        if (name == null || name.length == 0)
+            return console.log('意外的 fg 行:' + row)
+        name = name[0]
+        if (name == 'none')
+            return
+        pgmArr.push('fgnull(1)')
+        pgmArr.push(`fg("${name}.png",0,0)`)
+        return true
+    }
+}
