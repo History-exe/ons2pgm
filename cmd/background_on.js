c570b7d782245adff32d668202223dceefa24449
History_exe <33642965+History-exe@users.noreply.github.com>
2018-05-11 18:16:34 +0800

    修改读取文件名逻辑，以适配PGM读取措施

--- cmd/background_on.js
+++ cmd/background_on.js
@@ -4,8 +4,12 @@ module.exports = function (row, pgmArr) {
         if (name == null || name.length == 0)
             return console.log('意外的 bg 行:' + row)
         name = name[0]
+
+        if (name.indexOf('/') != -1)
+            name = name.match(/(?<=\/).*/)
         if (name == 'none')
             return
+
         pgmArr.push('bgnull(1)')
         pgmArr.push(`bg("${name}.png")`)
         pgmArr.push('bgon(1)')
