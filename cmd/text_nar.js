1a0c8b66e97736082b0647e224ff949a90bcb696
History_exe <33642965+History-exe@users.noreply.github.com>
2018-05-11 19:35:16 +0800

    修复旁白冒号

--- cmd/text_nar.js
+++ cmd/text_nar.js
@@ -2,7 +2,7 @@ module.exports = function (row, pgmArr) {
     if (row.substring(0, 1).search(/[\u4e00-\u9fa5]/) != -1) {
         var data = row.match(/.*/)
 
-        pgmArr.push(`text(${data}")`)
+        pgmArr.push(`text("${data}")`)
         pgmArr.push(`textcl()`)
 
         return true
