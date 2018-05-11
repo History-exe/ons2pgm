95c12977e9e18bf53ced9d24f1bb84152c21ba25
History_exe <33642965+History-exe@users.noreply.github.com>
2018-05-11 19:00:13 +0800

    用比较激进的办法写了关闭背景

--- cmd/background_off.js
+++ cmd/background_off.js
@@ -1,6 +1,17 @@
 module.exports = function (row, pgmArr) {
-
     if (row.substring(0, 18) == 'bg01 "file","none"') {
+        pgmArr.push('bgnull(1)')
+        return true
+    }
+    if (row.substring(0, 18) == 'bg02 "file","none"') {
+        pgmArr.push('bgnull(1)')
+        return true
+    }
+    if (row.substring(0, 19) == 'bg01 "show","false"') {
+        pgmArr.push('bgnull(1)')
+        return true
+    }
+    if (row.substring(0, 19) == 'bg02 "show","false"') {
         pgmArr.push('bgnull(1)')
         return true
     }
